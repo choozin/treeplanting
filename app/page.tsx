b6799c7b460872f79a0aba65f4f0f5d162218d79
Cam <ctaylor17@outlook.com>
2025-03-02 04:56:00 -0500

    Header

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import RecipesList from '../components/Recipes/RecipeList';
 export default function HomePage() {
   return (
     <>
+      <div>This page is a bit chaotic right now... working on several different features at once. If you're looking for the original table I had at this link let me know and I'll throw it back up here.</div>
       <AuthForm/>
       <RecipesList/>
       <CalendarViews/>
